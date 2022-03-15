9081bd75ac9192d481cdc14266c9748fa6f1a098
allenkirby0 <ark3@email.sc.edu>
2022-03-15 14:17:21 -0400

    Added Method Stubs

--- flightplanner/dataReader.java
+++ flightplanner/dataReader.java
@@ -1,5 +1,18 @@
 package flightplanner;
 
+import java.util.ArrayList;
+
 public class dataReader {
-    
+    public ArrayList<Account> getAccount(){
+        return null;
+    }
+    public ArrayList<Flight> getFlight(){
+        return null;
+    }
+    public ArrayList<Hotel> getHotel(){
+        return null;
+    }
+    public ArrayList<Ticket> getTickets(){
+        return null;
+    }
 }
--- flightplanner/dataWriter.java
+++ flightplanner/dataWriter.java
@@ -1,5 +1,16 @@
 package flightplanner;
 
 public class dataWriter {
-    
+    public void saveAccount(){
+        //TODO
+    }
+    public void saveFlight(){
+        //TODO
+    }
+    public void saveHotel(){
+        //TODO
+    }
+    public void saveTicket(){
+        //TODO
+    }
 }
