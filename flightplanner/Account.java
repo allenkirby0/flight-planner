07bc69013c836b86b3cddb30431d26abc983bfee
allenkirby0 <ark3@email.sc.edu>
2022-03-15 14:10:50 -0400

    Added Method Stubs

--- flightplanner/Account.java
+++ flightplanner/Account.java
@@ -1,5 +1,80 @@
 package flightplanner;
 
+import java.util.ArrayList;
+
 public class Account {
+    private String firstName;
+    private String lastName;
+    private int acctNum;
+    private String userName;
+    private String password;
+    private String DOB;
+    private int passportNum;
+    private String userEmail;
+    private int userPhone;
+
+    public Account(String firstName, String lastName, int acctNum, String userName, String password, String DOB, int passportNum, String userEmail, int userPhone){
+        //TODO Constructor
+    }
+
+    public void getUserInfo(){
+        //TODO
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAcctNum(){
+        return acctNum;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public String getPassword(){
+        return password;
+    }
+    public String getDOB(){
+        return DOB;
+    }
+    public int getPassportNum(){
+        return passportNum;
+    }
+    public String getUserEmail(){
+        return userEmail;
+    }
+    public int getPhone(){
+        return userPhone;
+    }
+
+    public void setFirstName(){
+        //TODO
+    }
+    public void setLastName(){
+        //TODO
+    }
+    public void setAcctNum(){
+        //TODO
+    }
+    public void setUserName(){
+        //TODO
+    }
+    public void setPassword(){
+        //TODO
+    }
+    public void setDOB(){
+        //TODO
+    }
+    public void setPassportNum(){
+        //TODO
+    }
+    public void setUserEmail(){
+        //TODO
+    }
+    public void setUserPhone(){
+        //TODO
+    }
     
 }
