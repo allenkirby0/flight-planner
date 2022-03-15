b3fb66e8f584173432b2dcb440e7e92ab9ad677d
allenkirby0 <ark3@email.sc.edu>
2022-03-15 13:53:07 -0400

    Added Hotel Method Stubs

--- flightplanner/Hotel.java
+++ flightplanner/Hotel.java
@@ -1,5 +1,45 @@
 package flightplanner;
 
 public class Hotel {
+
+    protected String hotelName;
+    protected Room hotelRoom;
+    protected String arriveDate;
+    protected String departDate;
+
+    public Hotel(String hotelName, Room hotelRoom, String arriveDate, String departDate){
+        //TODO Constructor
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public Room getHotelRoom(){
+        return hotelRoom;
+    }
     
+    public String getArrive(){
+        return arriveDate;
+    }
+
+    public String getDepart(){
+        return departDate;
+    }
+
+    public void setName(String hotelName){
+        //TODO
+    }
+
+    public void setRoom(Room hotelRoom){
+        //TODO
+    }
+
+    public void setArrive(String arriveDate){
+        //TODO
+    }
+    
+    public void setDepart(String departDate){
+        
+    }
 }
--- flightplanner/Room.java
+++ flightplanner/Room.java
@@ -1,5 +1,24 @@
 package flightplanner;
 
 public class Room {
-    
+    public int roomNum;
+    public String roomType;
+
+    public Room(int roomNum, String roomType){
+        //TODO Constructor
+    }
+
+    public int getRoomNum(){
+        return roomNum;
+    }
+    public String getRoomType(){
+        return roomType;
+    }
+
+    public void setRoomNum(){
+        //TODO
+    }
+    public void setRoomType(){
+        //TODO
+    }
 }
