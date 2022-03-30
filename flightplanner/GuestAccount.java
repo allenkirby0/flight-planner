47a5d7cd4bfade7f2d44f8fc272ce535e35db295
Matthew Jensen <mattydjensen@gmail.com>
2022-03-30 16:43:36 -0400

    added empty guestaccont ctor

--- flightplanner/GuestAccount.java
+++ flightplanner/GuestAccount.java
@@ -5,4 +5,8 @@ public class GuestAccount extends Account{
     public GuestAccount(String firstName, String lastName, String dob, String passportNum, String userEmail, String userPhone) {
         super(firstName, lastName, dob, passportNum, userEmail, userPhone);
     }
+
+    public GuestAccount() {
+        
+    }
 }
