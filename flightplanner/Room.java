f8a1df99736c9dbbde58d3a1b5f6c4548186a929
GElsey55 <g2elsey@aol.com>
2022-03-31 17:11:14 -0400

    Added Javadoc

--- flightplanner/Room.java
+++ flightplanner/Room.java
@@ -1,26 +1,50 @@
 package flightplanner;
 
+/**
+ * Room class
+ */
 public class Room {
     public int roomNum;
     public String roomType;
 
+    /**
+     * Constructor for Room
+     * @param roomNum
+     * @param roomType
+     */
     public Room(int roomNum, String roomType){
         this.roomNum = roomNum;
         this.roomType = roomType;
     }
 
+    /**
+     * returns roomNum variable
+     * @return
+     */
     public int getRoomNum(){
         return roomNum;
     }
 
+    /**
+     * returns roomType Variable
+     * @return
+     */
     public String getRoomType(){
         return roomType;
     }
 
+    /**
+     * sets roomNum variable
+     * @param roomNum
+     */
     public void setRoomNum(int roomNum){
         this.roomNum = roomNum;
     }
 
+    /**
+     * sets roomType variable
+     * @param roomType
+     */
     public void setRoomType(String roomType){
         this.roomType = roomType;
     }
