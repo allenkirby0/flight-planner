bd215feab700c89deca3c8bae10f08930e9d9510
GElsey55 <g2elsey@aol.com>
2022-03-31 14:13:49 -0400

    Implemented hotels class with javadoc

--- flightplanner/Hotels.java
+++ flightplanner/Hotels.java
@@ -1,5 +1,47 @@
 package flightplanner;
 
+import java.util.ArrayList;
+/**
+ * Hotels class
+ */
 public class Hotels {
     
+    private static Hotels hotels = null;
+    private static ArrayList<Hotel> hotelList;
+
+    /**
+     * Loads list of hotels from json
+     */
+    private Hotels() {
+        hotelList = DataReader.loadHotel();
+    }
+
+    /**
+     * Creates a new instance of Hotels if one does not already exist
+     * @return Hotels instance
+     */
+    private static Hotels getInstance() {
+
+        if (hotels == null) {
+            hotels = new Hotels();
+        }
+
+        return hotels;
+    }
+
+    /**
+     * Returns an arraylist of hotels
+     * @return
+     */
+    public ArrayList<Hotel> getHotels() {
+        return hotelList;
+    }
+
+    /**
+     * Adds a hotel to the hotel list
+     * @param hotel a hotel
+     */
+    public void addHotel(Hotel hotel) {
+        hotelList.add(hotel);
+    }
 }
