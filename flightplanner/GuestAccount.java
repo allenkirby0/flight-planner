f2fc60e768cbaa3459d4b0e0b0b528d319ccb71f
GElsey55 <g2elsey@aol.com>
2022-03-30 23:51:45 -0400

    Added javadoc for methods

--- flightplanner/GuestAccount.java
+++ flightplanner/GuestAccount.java
@@ -1,10 +1,27 @@
 package flightplanner;
 
+/**
+ * GuestAccount class that holds temporary
+ * user information 
+ */
 public class GuestAccount extends Account{
     
+    /**
+     * Empty Constructor
+     */
     public GuestAccount() {
         super();
     }
+
+    /**
+     * GuestAccount Constructor
+     * @param firstName
+     * @param lastName
+     * @param dob
+     * @param passportNum
+     * @param userEmail
+     * @param userPhone
+     */
     public GuestAccount(String firstName, String lastName, String dob, String passportNum, String userEmail, String userPhone) {
         super(firstName, lastName, dob, passportNum, userEmail, userPhone);
     }
--- flightplanner/MemberAccount.java
+++ flightplanner/MemberAccount.java
@@ -1,12 +1,28 @@
 package flightplanner;
 import java.util.UUID;
 
+/**
+ * MemberAccount class that holds a lot of
+ * user information
+ */
 public class MemberAccount extends Account {
 
     private String acctNum;
     private String username;
     private String password;
 
+    /**
+     * MemberAccount Constructor
+     * @param firstName
+     * @param lastName
+     * @param acctNum
+     * @param username
+     * @param password
+     * @param dob
+     * @param passportNum
+     * @param userEmail
+     * @param userPhone
+     */
     public MemberAccount (String firstName, String lastName,String acctNum, String username, String password, String dob, String passportNum, String userEmail, String userPhone) {
         //Set all 
         super(firstName, lastName, dob, passportNum, userEmail, userPhone);
@@ -16,26 +32,50 @@ public class MemberAccount extends Account {
 
     }
 
+    /**
+     * returns AcctNum variable
+     * @return acctNum
+     */
     public String getAcctNum () {
         return acctNum;
     }
 
+    /**
+     * sets AcctNum variable
+     * @param acctNum
+     */
     public void setAcctNum (String acctNum) {
         this.acctNum = acctNum;
     }
 
+    /**
+     * returns Username variable
+     * @return username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * sets Username variable
+     * @param username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * returns Password variable
+     * @return
+     */
     public String getPassword() {
         return password;
     }
     
+    /**
+     * sets Password variable
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
