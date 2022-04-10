99f4b1c390cf399370b768bcf916dca1fc8eaf58
GElsey55 <g2elsey@aol.com>
2022-04-10 19:47:34 -0400

    Added ticket test

---
+++ flightplanner/TicketTest.java
@@ -0,0 +1,32 @@
+package flightplanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TicketTest {
+    
+    @BeforeClass
+	public static void oneTimeSetup() {
+		
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() {
+		
+	}
+	
+	@BeforeEach
+	public static void setup() {
+		//runs before each test
+	}
+	
+	@AfterEach
+	public static void tearDown() {
+		//runs after each test
+	}
+}
