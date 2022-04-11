1cfed9e858242ceaa8d4adc7f3f19a4bd2f32c9e
Matthew Jensen <mattydjensen@gmail.com>
2022-04-10 22:48:44 -0400

    added HotelTest

--- flightplanner/HotelTest.java
+++ flightplanner/HotelTest.java
@@ -2,6 +2,8 @@ package flightplanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.AfterEach;
@@ -10,4 +12,24 @@ import org.junit.jupiter.api.Test;
 
 public class HotelTest {
     
+    @BeforeClass
+	public static void oneTimeSetup() {
+		Hotels hotels = Hotels.getInstance();
+	}
+	
+	@AfterClass
+	public static void oneTimeTearDown() {
+		
+	}
+	
+	@BeforeEach
+	public static void setup() {
+		//runs before each test
+	}
+	
+	@AfterEach
+	public static void tearDown() {
+		//runs after each test
+	}
+
 }
