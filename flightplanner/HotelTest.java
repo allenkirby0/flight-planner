1dc87ad7dbaa423cb9327e478f2a97ba9e8d2787
Matthew Jensen <mattydjensen@gmail.com>
2022-04-10 21:42:08 -0400

    add test hotel skeleton

---
+++ flightplanner/HotelTest.java
@@ -0,0 +1,13 @@
+package flightplanner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class HotelTest {
+    
+}
